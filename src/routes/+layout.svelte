2965b673f9c0a167a5e8c98906dacb2283615f3e
eriolay <126813646+eriolay@users.noreply.github.com>
2026-03-19 10:54:09 +0100

    finished lab 4

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,9 +11,33 @@
         {url: "/contact", title: "Contact"},
         {url: "https://github.com/eriolay", title: "Github"}
 ];
+
+let colorScheme = "light dark";
+let root = globalThis.document?.documentElement;
+$: root?.style.setProperty("color-scheme", colorScheme);
+
+// let colorScheme = "light dark"; // default colorScheme value
+let localStorage = globalThis.localStorage ?? {};
+
+
+if (localStorage.colorScheme) { // if localStorage has a colorScheme property
+  colorScheme = localStorage.colorScheme; // override the default colorScheme
+}
+
+$: localStorage.colorScheme = colorScheme; // whenever colorScheme changes, update localStorage
+
+
 </script>
 
 
+<label class="color-scheme-switch">
+    Theme:
+    <select bind:value={ colorScheme }>
+        <option value="light dark">Automatic</option>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+    </select>
+</label>
 
 
 <nav>
@@ -38,15 +62,51 @@
     </ul>
 </nav>
 
-<!-- 
-<nav>
-        <ul>
-            <li><a href=".">Home</a></li>
-            <li><a class="current" href="projects">Projects</a></li>
-            <li><a href="contact">Contact</a></li>
-            <li><a href="resume">My Resume</a></li>
-            <li><a href="https://github.com/eriolay" target="_blank">My GitHub</a></li>
-        </ul>
-    </nav> -->
+
+
+<style>
+
+:root{
+    color-scheme: dark;
+}
+
+    label.color-scheme-switch {
+        position: absolute;
+        top: 1rem;
+        right: 5rem;
+        display: inline-flex;
+        gap: 4px;
+        font-size: 80%;
+    }
+    nav {
+        --border-color: oklch(50% 10% 200 / 40%);
+        display: flex;
+        /* border-bottom-width: 1px;
+        border-bottom-style: solid;
+        border-bottom-color: oklch(80% 3% 200); */
+        border-bottom: 2px solid var(--border-color);
+        margin-bottom: 1em;
+    } 
+    
+    a:hover {
+        border-bottom-color: var(--color-accent);
+        border-bottom-width: 0.4em;
+        border-bottom-style: solid;
+        /* background-color: oklch(from var(--color-accent) 95% 5% h); */
+        background-color: color-mix(in oklch, var(--color-accent), canvas 85%);
+
+    }
+
+    a.current {
+        flex: 1;
+        /* border-bottom-width: 0.4em;
+        border-bottom-style: solid;
+        border-bottom-color: oklch(80% 3% 200);    */
+        border-bottom: 4px solid var(--border-color); 
+        }
+
+    
+</style>
+
 
 <slot />
