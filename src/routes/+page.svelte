9589980e35c24db2437e527bdafad11daf818810
eriolay <126813646+eriolay@users.noreply.github.com>
2026-03-21 20:22:09 +0000

    building up the projects page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,40 @@
   import readings from "$lib/reading.json";
   import ReadingItem from "$lib/ReadingItem.svelte";
 
+
+import { onMount } from "svelte";
+
+let githubData = null; // This will eventually hold our Github stats
+let loading = true; // This will be true *until* the fetch's promise resolves to a value
+let error = null; // If the API call resulted in an error, it will go into this variable
+
+onMount(async () => {
+     try {
+        console.log("Page has been mounted!")
+        // let response = await fetch("https://api.github.com/users/eriolay");
+
+        let response = await {
+            ok: true,
+            json: async () => ({
+                followers: 100,
+                following: 100,
+                public_repos: 100,
+                public_gists: 100,
+            }),
+            };
+
+        console.log(response);
+        githubData = await response.json();
+        console.log(githubData);
+    } catch (err) {
+        error = err;
+    }
+    loading = false;
+})
+
+
 </script>
+
 <svelte:head>
   
 </svelte:head>
@@ -19,6 +52,29 @@
     </p>
     <img src="images/headshot.jpg" alt="Me against a background of red bleachers"
     style="width:600px;height:400px;">
+
+
+    {#if loading}
+        <p>Loading...</p>
+    {:else if error}
+        <p>Something went wrong: {error.message}</p>
+    {:else}
+        <!-- The data is {JSON.stringify(githubData)} -->
+         <section>
+            <h2>My Github Stats</h2>
+            <dl>
+                <dt>Followers: </dt>
+                <dd>{githubData.followers}</dd>
+                <dt>Following: </dt>
+                <dd>{githubData.following}</dd>
+                <dt>Public Repos: </dt>
+                <dd>{githubData.public_repos}</dd>
+                <dt>Public Gists: </dt>
+                <dd>{githubData.public_gists}</dd>
+            </dl>
+         </section>
+    {/if}
+
 
     <h2>Most Recent Projects</h2>
     <div class="projects-highlights">
@@ -35,3 +91,18 @@
     </div>
 
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+
+<style>
+    dl {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);  
+    }
+
+    dt {
+        font-weight: bold;
+        /* color:rgb(221, 196, 164); */
+        text-transform: uppercase;
+    }
+</style>
